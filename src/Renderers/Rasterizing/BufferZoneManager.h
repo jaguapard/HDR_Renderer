6e25b28614e9d5f4dfec39b5a636bcc8d4112e1c
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-04-21 17:00:26 +0500

    BufferZoneManage: added default ctor

--- src/Renderers/Rasterizing/BufferZoneManager.h
+++ src/Renderers/Rasterizing/BufferZoneManager.h
@@ -4,6 +4,7 @@ namespace Rasterizing
 	class BufferZoneManager
 	{
 	public:
+		BufferZoneManager() {};
 		BufferZoneManager(int threadCount, int w, int h);
 		//Writes out threads responsible for this bounding box into out. Returns recorded element count. out must be large enough to store at least threadCount elements 
 		int getThreadsResponsible(int* out, float minX, float minY, float maxX, float maxY) const;
