23bc083d7ca99f19511f98abc11b5e31b0685f8d
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-04-21 16:27:13 +0500

    Very primitive BufferZoneManager (unused)

---
+++ src/Renderers/Rasterizing/BufferZoneManager.h
@@ -0,0 +1,14 @@
+#pragma once
+namespace Rasterizing
+{
+	class BufferZoneManager
+	{
+	public:
+		BufferZoneManager(int threadCount, int w, int h);
+		//Writes out threads responsible for this bounding box into out. Returns recorded element count. out must be large enough to store at least threadCount elements 
+		int getThreadsResponsible(int* out, float minX, float minY, float maxX, float maxY) const;
+		void getLimitsForThread(int threadIndex, float& minX, float& minY, float& maxX, float& maxY) const;
+	private:
+		int threadCount, w, h;
+	};
+}
