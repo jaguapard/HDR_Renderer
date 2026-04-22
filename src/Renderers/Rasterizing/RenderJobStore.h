974521949549b3c8ce24ac44139ef6cf9eca0a73
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-04-22 16:32:57 +0500

    Removed RenderJobStore

--- src/Renderers/Rasterizing/RenderJobStore.h
+++ src/Renderers/Rasterizing/RenderJobStore.h
@@ -23,23 +23,4 @@ namespace Rasterizing
 	struct VertexPack16;
 	struct DrawCommand;
 	class Vertice_Store;
-	//uses block list to store render jobs with stable pointers
-	struct RenderJobStore
-	{
-		RenderJobStore() = default;
-		RenderJobStore(const Rasterizing::Vertice_Store* vertStore);
-		//std::array<VertexPack16, 3> loadVertices16(size_t firstInd, Mask16 mask) const;
-		//VertexPack16 gatherVertices16(int32x16 indices) const;
-
-		void addMany(const VertexPack16* pStart, const VertexPack16* pEnd, const float32x16& rcpSignedArea, const int32x16& diffuseMapIndex, Mask16 activeElementsMask, const DrawCommand& subInfo, const int32x16* vertexIndexStart, const int32x16* vertexIndexEnd, bool areClipped);
-
-		size_t size() const;
-		RenderJob operator[](size_t i);
-		void reset();
-		void clear();
-		const Rasterizing::Vertice_Store* verticeStore = nullptr;
-	private:
-		BlockStore<RenderJob, 1024> heavyRenderJobs;
-		BlockStore<RenderJobLight, 8192> lightRenderJobs;
-	};
 }
