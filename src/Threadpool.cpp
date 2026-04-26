4699637637f85d4cc0c18addb3a5201aceb39fdb
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-04-26 12:43:29 +0500

    Threadpool rewrite: handle and task types moved inside Threadpool class and renamed. GenericHandle is now hidden from users. DepedencyStore remade into WaitableCollection. Now can only block on waitable handles or collections. Added help while waiting. Readded single thread mode and using it in debug. markTaskFinished now automatically signals waiting threads. Added non-overlapping partition function

--- src/Threadpool.cpp
+++ src/Threadpool.cpp
@@ -11,8 +11,9 @@ Threadpool::Threadpool(size_t numThreads)
 	size_t threadCount = numThreads;
 	if (!numThreads)
 	{
-		threadCount = std::max(1u, std::thread::hardware_concurrency() - 1);
+		threadCount = std::max<int64_t>(1, int64_t(std::thread::hardware_concurrency()) - 1); //avoid crowding out the main thread unless that's impossible. Also, hardware_concurrency() can return 0
 	}
+	if (SINGLE_THREAD_MODE) threadCount = 1;
 	//threadCount = 24;
 	this->spawnThreads(threadCount);
 }
@@ -25,7 +26,7 @@ void Threadpool::spawnThreads(size_t threadCount)
 		this->workers[i] = std::jthread(&Threadpool::workerRoutine, this, i);
 }
 
-std::optional<std::pair<uint64_t, ThreadpoolTask>> Threadpool::tryPopTask()
+std::optional<std::pair<uint64_t, Threadpool::Task>> Threadpool::tryPopTask()
 {
 	std::lock_guard lck(this->structs_mtx);
 	for (auto& it : this->unassignedTasks)
@@ -49,12 +50,16 @@ std::optional<std::pair<uint64_t, ThreadpoolTask>> Threadpool::tryPopTask()
 
 void Threadpool::markTaskFinished(uint64_t id)
 {
-	std::lock_guard lck(this->structs_mtx);
-	assert(this->inProgressTaskIds.find(id) != this->inProgressTaskIds.end());
-	this->inProgressTaskIds.erase(id);
+	{
+		std::lock_guard lck(this->structs_mtx);
+		assert(this->inProgressTaskIds.find(id) != this->inProgressTaskIds.end());
+		this->inProgressTaskIds.erase(id);
+	}
+	std::lock_guard lck(cv_mtx);
+	cv.notify_all();
 }
 
-TaskHandle Threadpool::addTask(const ThreadpoolTask& task)
+Threadpool::TaskHandle Threadpool::addTask(const Threadpool::Task& task)
 {
 	uint64_t id = this->lastFreeHandle++;
 	{
@@ -70,7 +75,7 @@ TaskHandle Threadpool::addTask(const ThreadpoolTask& task)
 	return h;
 }
 
-GateHandle Threadpool::createGate()
+Threadpool::GateHandle Threadpool::createGate()
 {
 	GateHandle ret;
 	ret.handle.pool = this;
@@ -81,29 +86,57 @@ GateHandle Threadpool::createGate()
 	return ret;
 }
 
-void Threadpool::blockUntilTasksComplete(const std::vector<TaskHandle>& tasks)
+void Threadpool::blockUntilComplete(const WaitableHandle& handle)
 {
-	for (auto& it : tasks)
-	{
-		this->blockUntilWaitableComplete(WaitableHandle(it));
-	}
-}
-
-void Threadpool::blockUntilWaitablesComplete(const std::vector<WaitableHandle>& waitables)
-{
-	for (auto& it : waitables) this->blockUntilWaitableComplete(it);
-}
-
-void Threadpool::blockUntilWaitableComplete(const WaitableHandle& waitable)
-{
-	if (bIsWorkerThread) throw std::runtime_error("Thread pool worker attempted to block on waitable " + std::to_string(waitable.handle.id) + ". Workers blocking on thread pool's waitables is prone to deadlocks and is not supported. Define dependecies while adding a task if you want the task to start after prerequisites are complete.");
-	if (waitable.handle.id >= this->lastFreeHandle) throw std::runtime_error("Attempting to wait on non-existant handle.");
-	if (waitable.isComplete()) return;
+	if (bIsWorkerThread) throw std::runtime_error("Thread pool worker attempted to block on waitable " + std::to_string(handle.handle.id) + ". Workers blocking on thread pool's waitables is prone to deadlocks and is not supported. Define dependecies while adding a task if you want the task to start after prerequisites are complete.");
+	if (handle.handle.id >= this->lastFreeHandle) throw std::runtime_error("Attempting to wait on non-existant handle.");
+	if (handle.isComplete()) return;
 
 	std::unique_lock cv_lck(cv_mtx);
 	this->cv.wait(cv_lck, [&]() {
-		return waitable.isComplete();
+		return handle.isComplete();
 		});
+}
+
+void Threadpool::blockUntilComplete(const WaitableCollection& collection)
+{
+	for (auto& it : collection) this->blockUntilComplete(it);
+}
+
+void Threadpool::helpWhileWaiting(const WaitableCollection& collection)
+{
+	for (auto& it : collection) this->helpWhileWaiting(it);
+}
+
+void Threadpool::helpWhileWaiting(const WaitableHandle& handle)
+{
+	std::optional<std::pair<uint64_t, Threadpool::Task>> backloggedTask;
+	while (true)
+	{
+		if (backloggedTask)
+		{
+			backloggedTask->second.func();
+			this->markTaskFinished(backloggedTask->first);
+			backloggedTask = std::nullopt;
+		}
+		if (handle.isComplete()) break;
+		else
+		{
+			auto popped = this->tryPopTask();
+			if (popped) //if there's a job - take it
+			{
+				backloggedTask = popped;
+			}
+			else //if not - wait until either one is available or task we're waiting for is finished
+			{
+				std::unique_lock cv_lck(this->cv_mtx);
+				cv.wait(cv_lck, [&]() {
+					backloggedTask = this->tryPopTask();
+					return backloggedTask.has_value() || handle.isComplete();
+					});
+			}
+		}
+	}
 }
 
 bool Threadpool::hasTaskFinished(const TaskHandle& task)
@@ -148,7 +181,7 @@ size_t Threadpool::getThreadCount() const
 	return this->getWorkerCount();
 }
 
-std::vector<TaskHandle> Threadpool::addTaskBatch(const std::vector<ThreadpoolTask>& tasks)
+std::vector<Threadpool::TaskHandle> Threadpool::addTaskBatch(const std::vector<Threadpool::Task>& tasks)
 {
 	size_t sz = tasks.size();
 	uint64_t id = this->lastFreeHandle.fetch_add(sz);
@@ -176,7 +209,7 @@ void Threadpool::workerRoutine(size_t myNumber)
 	bIsWorkerThread = true;
 	while (true)
 	{
-		std::optional<std::pair<uint64_t, ThreadpoolTask>> taskForMe;
+		std::optional<std::pair<uint64_t, Threadpool::Task>> taskForMe;
 		{
 			std::unique_lock cv_lck(cv_mtx);
 			cv.wait(cv_lck, [&]() {
@@ -187,37 +220,52 @@ void Threadpool::workerRoutine(size_t myNumber)
 
 		taskForMe->second.func();
 		this->markTaskFinished(taskForMe->first);
-
-		std::unique_lock cv_lck(cv_mtx);
-		cv.notify_all();
 	}
 }
 
-std::pair<double, double> Threadpool::getLimitsForThread(size_t threadIndex, double min, double max, std::optional<size_t> threadCount) const
+std::pair<double, double> Threadpool::getLimitsForThread(size_t threadIndex, double min, double max, size_t threadCount) const
 {
-	size_t nThreads = threadCount ? threadCount.value() : this->getWorkerCount();
+	assert(min >= max);
+	size_t nThreads = threadCount ? threadCount : this->getWorkerCount();
 	double minLimit = lerp(min, max, double(threadIndex) / nThreads);
 	double maxLimit = lerp(min, max, double(threadIndex + 1) / nThreads);
 	return std::make_pair(std::clamp(minLimit, min, max), std::clamp(maxLimit, min, max));
 }
-WaitableHandle::WaitableHandle(const TaskHandle& task)
+std::vector<std::pair<uint64_t, uint64_t>> Threadpool::getNonOverlappingPartitions(uint64_t min, uint64_t max, uint64_t count) const
+{
+	count = count ? count : this->getWorkerCount();
+	assert(max >= min);
+	assert(count > 0);
+	std::vector<std::pair<uint64_t, uint64_t>> ret;
+	uint64_t remaining = max - min, partStart = min;
+	ret.reserve(count);
+	for (uint64_t i = 0; i < count; ++i)
+	{
+		uint64_t part = remaining / (count - i);
+		ret.push_back(std::make_pair(partStart, partStart + part));
+		partStart += part;
+		remaining -= part;
+	}
+	return ret;
+}
+Threadpool::WaitableHandle::WaitableHandle(const TaskHandle& task)
 {
 	this->handle = task.handle;
 	this->type = Type::TASK;
 }
 
-WaitableHandle::WaitableHandle(const GateHandle& gate)
+Threadpool::WaitableHandle::WaitableHandle(const GateHandle& gate)
 {
 	this->handle = gate.handle;
 	this->type = Type::GATE;
 }
 
-void WaitableHandle::blockUntilComplete()
+void Threadpool::WaitableHandle::blockUntilComplete()
 {
-	handle.pool->blockUntilWaitableComplete(*this);
+	handle.pool->blockUntilComplete(*this);
 }
 
-bool WaitableHandle::isComplete() const
+bool Threadpool::WaitableHandle::isComplete() const
 {
 	switch (this->type)
 	{
@@ -226,17 +274,17 @@ bool WaitableHandle::isComplete() const
 	}
 }
 
-bool TaskHandle::hasFinished() const
+bool Threadpool::TaskHandle::hasFinished() const
 {
 	return handle.pool->hasTaskFinished(*this);
 }
 
-void GateHandle::open()
+void Threadpool::GateHandle::open()
 {
 	handle.pool->openGate(*this);
 }
 
-bool GateHandle::isOpen() const
+bool Threadpool::GateHandle::isOpen() const
 {
 	return handle.pool->isGateOpen(*this);
 }
--- src/Threadpool.h
+++ src/Threadpool.h
@@ -14,122 +14,134 @@
 typedef std::function<void()> taskfunc_t;
 
 class Threadpool;
-class TaskHandle;
-class GateHandle;
-class WaitableHandle;
-class GenericHandle
-{
-public:
-	friend class Threadpool;
-	friend class TaskHandle;
-	friend class GateHandle;
-	friend class WaitableHandle;
-protected:
-	uint64_t id = 0;
-	Threadpool* pool = nullptr;
-};
-class TaskHandle
-{
-public:
-	friend class Threadpool;
-	friend class WaitableHandle;
 
-	//TaskHandle() = delete;
-	bool hasStarted() const;
-	bool hasFinished() const;
-	bool isInProgress() const;
-private:
-	GenericHandle handle;
-};
-
-class GateHandle
-{
-public:
-	friend class Threadpool;
-	friend class WaitableHandle;
-
-	//GateHandle() = delete;
-	void open();
-	bool isOpen() const;
-private:
-	GenericHandle handle;
-};
-
-class WaitableHandle
-{
-public:
-	friend class Threadpool;
-
-	//WaitableHandle() = delete;
-	WaitableHandle(const TaskHandle& task);
-	WaitableHandle(const GateHandle& gate);
-	void blockUntilComplete();
-	bool isComplete() const;
-private:
-	enum class Type
-	{
-		TASK,
-		GATE
-	};
-	GenericHandle handle;
-	Type type;
-};
-
-struct DependencyStore
-{
-	std::vector<WaitableHandle> store;
-	DependencyStore() {};
-	
-	template<typename T> DependencyStore(const std::vector<T>& v) 
-	{ 
-		for (auto& it : v) store.emplace_back(it); 
-	}
-	template<typename T> DependencyStore& operator=(const std::vector<T>& v) 
-	{ 
-		store.clear(); 
-		for (auto& it : v) store.emplace_back(it); 
-		return *this; 
-	}
-
-	size_t size() const { return store.size(); }
-	void clear() { store.clear(); }
-	template<typename T> WaitableHandle& emplace_back(const T& t) { return store.emplace_back(t); }
-};
-
-struct ThreadpoolTask
-{
-	ThreadpoolTask() {};
-	ThreadpoolTask(const taskfunc_t& func, const DependencyStore& dependencies = {}) : func(func), dependencies(dependencies) {};
-	taskfunc_t func;
-	DependencyStore dependencies; //Forces the task to be considered runnable only if all the input handles complete.
-};
 class Threadpool
 {
+private:
+	class GenericHandle
+	{
+	public:
+		GenericHandle() = default;
+		uint64_t id = 0;
+		Threadpool* pool = nullptr;
+	};
 public:
-	/*
-	friend class GenericHandle;
-	friend class TaskHandle;
-	friend class GateHandle;
-	friend class WaitableHandle;*/
+	class TaskHandle
+	{
+	public:
+		friend class Threadpool;
+		friend class WaitableHandle;
+
+		//TaskHandle() = delete;
+		bool hasStarted() const;
+		bool hasFinished() const;
+		bool isInProgress() const;
+	private:
+		static inline constexpr bool isThreadpoolHandleType = true;
+		GenericHandle handle;
+	};
+
+	class GateHandle
+	{
+	public:
+		friend class Threadpool;
+		friend class WaitableHandle;
+		//GateHandle() = delete;
+		void open();
+		bool isOpen() const;
+	private:
+		static inline constexpr bool isThreadpoolHandleType = true;
+		GenericHandle handle;
+	};
+
+	class WaitableHandle
+	{
+	public:
+		friend class Threadpool;
+
+		//WaitableHandle() = delete;
+		WaitableHandle() = default;
+		WaitableHandle(const TaskHandle& task);
+		WaitableHandle(const GateHandle& gate);
+		void blockUntilComplete();
+		bool isComplete() const;
+	private:
+		static inline constexpr bool isThreadpoolHandleType = true;
+		enum class Type
+		{
+			TASK,
+			GATE
+		};
+		GenericHandle handle;
+		Type type;
+	};
+
+	struct WaitableCollection
+	{
+		std::vector<WaitableHandle> store;
+		WaitableCollection() {};
+
+		template<typename T> WaitableCollection(const std::vector<T>& v)
+		{
+			static_assert(T::isThreadpoolHandleType, "Waitable collection can only be constructed from Threadpool handle types");
+			for (auto& it : v) store.emplace_back(it);
+		}
+		template<typename T> WaitableCollection& operator=(const std::vector<T>& v)
+		{
+			static_assert(T::isThreadpoolHandleType, "Waitable collection can only be assigned to Threadpool handle types");
+			store.clear();
+			for (auto& it : v) store.emplace_back(it);
+			return *this;
+		}
+
+		auto begin() { return store.begin(); }
+		auto end() { return store.end(); }
+		const auto begin() const { return store.begin(); }
+		const auto end() const { return store.end(); }
+
+		size_t size() const { return store.size(); }
+		void clear() { store.clear(); }
+		template<typename T> WaitableHandle& emplace_back(const T& t) { 
+			static_assert(T::isThreadpoolHandleType, "Waitable collection can only be empaced to by Threadpool handle types"); 
+			return store.emplace_back(t); }
+		template<typename T> void push_back(const T& t) { return store.push_back(WaitableHandle(t)); }
+	};
+
+	struct Task
+	{
+		Task() {};
+		Task(const taskfunc_t& func, const WaitableCollection& dependencies = {}) : func(func), dependencies(dependencies) {};
+		taskfunc_t func;
+		WaitableCollection dependencies; //Forces the task to be considered runnable only if all the input handles complete.
+	};
+public:
+#ifdef NDEBUG
+	static constexpr bool SINGLE_THREAD_MODE = false;
+#else
+	static constexpr bool SINGLE_THREAD_MODE = true;
+#endif
 	Threadpool(size_t numThreads = 0);
 
 	//Adds task batch to the thread pool. Returns handles to tasks in the same order as in tasks vector.
-	std::vector<TaskHandle> addTaskBatch(const std::vector<ThreadpoolTask>& tasks);
+	std::vector<TaskHandle> addTaskBatch(const std::vector<Task>& tasks);
 
 	//Adds a single task to the threadpool and returns a handle for it
-	TaskHandle addTask(const ThreadpoolTask& task);
+	TaskHandle addTask(const Threadpool::Task& task);
 
 	//Creates a new gate and returns a handle for it. The gates can be used as dependencies for tasks to prevent then from being ran before the gate opens
 	GateHandle createGate();
 
-	//Blocks the calling thread until all passed tasks are complete. Blocking is unsupported on worker threads and will throw an exception if it gets called by one.
-	void blockUntilTasksComplete(const std::vector<TaskHandle>& tasks);
+	//Blocks the calling thread until waitable handle signals completion.  Blocking is unsupported on worker threads and will throw an exception if it gets called by one. Use helpWhileWaiting instead
+	void blockUntilComplete(const WaitableHandle& handle);
 
-	void blockUntilWaitablesComplete(const std::vector<WaitableHandle>& waitables);
+	//Blocks the calling thread until all waitable handles have signaled completion. Blocking is unsupported on worker threads and will throw an exception if it gets called by one. Use helpWhileWaiting instead
+	void blockUntilComplete(const WaitableCollection& collection);
 
-	void blockUntilWaitableComplete(const WaitableHandle& waitable);
-	//Makes the calling thread execute other tasks until input tasks are completed. Care should be taken, since it may hang the caller for indeterminate amount of time
-	//void helpWhileWaiting(const std::vector<TaskHandle>& tasks);
+	//Makes the calling thread execute other tasks until input task is completed. Care should be taken, since it may hang the caller for indeterminate amount of time
+	void helpWhileWaiting(const WaitableHandle& handle);
+
+	//Makes the calling thread execute other tasks until input tasks are completed. Care should be taken, since it may hang the caller for indeterminate amount of time.
+	void helpWhileWaiting(const WaitableCollection& collection);
 
 	bool hasTaskFinished(const TaskHandle& task);
 	bool isGateOpen(const GateHandle& gate);
@@ -140,11 +152,20 @@ public:
 	//Returns true if this function is called by threadpool's worker thread
 	bool isWorkerThread() const;
 
+	//Returns the worker thread count
 	size_t getWorkerCount() const;
+
+	//Returns the worker thread count
 	size_t getThreadCount() const;
 
-	std::pair<double, double> getLimitsForThread(size_t threadIndex, double min, double max, std::optional<size_t> threadCount = std::nullopt) const;
+	//Returns evenly-split limits of quantity for given thread count. If count is 0, the threadpool's worker count is used instead.
+	std::pair<double, double> getLimitsForThread(size_t threadIndex, double min, double max, size_t threadCount = 0) const;
 
+	//Returns count ranges as partitions of a given integer range min (inclusive) to max (excusive). If count is 0, the threadpool's worker count is used instead.
+	//Partitions cover entire given range with no gaps or overlaps. All subranges returned differ at most by 1 in their size.
+	//First element of each returned pair signifies range's start (inclusive). Second element of each returned pair signifies range's end (exclusive)
+	//The results are valid only if max >= min. Some returned intervals may be empty (pair.first == pair.second) if (max - min) < count
+	std::vector<std::pair<uint64_t, uint64_t>> getNonOverlappingPartitions(uint64_t min, uint64_t max, uint64_t count = 0) const;
 	static inline Threadpool* instance = nullptr;
 private:
 	std::unique_ptr<std::jthread[]> workers;
@@ -155,12 +176,12 @@ private:
 	mutable std::mutex cv_mtx;
 
 	mutable std::recursive_mutex structs_mtx;
-	std::unordered_map<uint64_t, ThreadpoolTask> unassignedTasks;
+	std::unordered_map<uint64_t, Task> unassignedTasks;
 	std::unordered_set<uint64_t> inProgressTaskIds;
 	std::unordered_set<uint64_t> closedGates;
 
 	void workerRoutine(size_t myNumber);
 	void spawnThreads(size_t threadCount);
-	std::optional<std::pair<uint64_t, ThreadpoolTask>> tryPopTask(); //if there are runnable tasks, then pops one and returns it. Else returns empty optional
+	std::optional<std::pair<uint64_t, Task>> tryPopTask(); //if there are runnable tasks, then pops one and returns it. Else returns empty optional
 	void markTaskFinished(uint64_t id);
 };
