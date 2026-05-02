ec96628bb51530024a8709be56c97cfff98771d9
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-05-03 03:59:00 +0500

    Looks like fully working mailbox system, with minor roof waviness issues and one unfilled shadow map pixel (master has it too)

--- src/Renderers/Rasterizing/RasterizingRenderer.cpp
+++ src/Renderers/Rasterizing/RasterizingRenderer.cpp
@@ -729,8 +729,8 @@ void RasterizingRenderer::drawTrianglePack(const TrianglePack16& pack, const uin
 	float32x16 my_xMin = 0.f;
 	float32x16 my_xMax = fRenderW - 1;
 	
-	float32x16 my_yMin = fRenderH / threadCount * threadIndex;
-	float32x16 my_yMax = (fRenderH / threadCount * (threadIndex + 1));
+	float32x16 my_yMin = _mm512_floor_ps(fRenderH / threadCount * threadIndex);
+	float32x16 my_yMax = _mm512_ceil_ps(fRenderH / threadCount * (threadIndex + 1));
 	my_yMax = _mm512_min_ps(my_yMax, fRenderH - 1);
 
 	float32x16 group_xBeg = _mm512_max_ps((my_xMin), pack.minX);
