56a551b858bd83604af0474e59cfe5b7e7a9e79f
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-05-03 05:10:20 +0500

    Fixed not filtering out self from pusing to peer batches

--- src/Renderers/Rasterizing/RasterizingRenderer.cpp
+++ src/Renderers/Rasterizing/RasterizingRenderer.cpp
@@ -608,6 +608,7 @@ void RasterizingRenderer::workerRoutine(const uint32_t threadIndex)
 					int maxThread = _mm512_reduce_max_epi32(_mm512_mask_mov_epi32(int32x16(INT32_MIN), trianglesForPeers, vecLastThread));
 					for (int i = minThread; i <= maxThread; ++i)
 					{
+						if (i == threadIndex) continue;
 						Mask16 trianglesForThreadI = activeTriangles & vecFirstThread <= i & vecLastThread >= i;
 						if (!trianglesForThreadI) continue;
 
