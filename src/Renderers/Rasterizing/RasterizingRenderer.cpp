faa32d79c391b67a55204b8159d2b35cf10f0fa7
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-05-05 04:19:06 +0500

    calculateBarycentricCoordinates2D/3D, mask_load_vec4_f32x16_from_framebuffer made static and moved to RendererBase class

--- src/Renderers/Rasterizing/RasterizingRenderer.cpp
+++ src/Renderers/Rasterizing/RasterizingRenderer.cpp
@@ -602,54 +602,6 @@ void RasterizingRenderer::binTrianglesIntoZones(int threadIndex)
 	}
 }
 
-__forceinline void calculateBarycentricCoordinates2D(const Vec4_f32x16& r, const Vec4_f32x16& r1, const Vec4_f32x16& r2, const Vec4_f32x16& r3, const float32x16& rcpSignedArea, float32x16& alpha, float32x16& beta, float32x16& gamma)
-{
-	alpha = (r - r3).cross2d(r2 - r3) * rcpSignedArea;
-	beta = (r - r3).cross2d(r3 - r1) * rcpSignedArea;
-	gamma = (r - r1).cross2d(r1 - r2) * rcpSignedArea; //do NOT change this to 1-alpha-beta or 1-(alpha+beta). That causes wonkiness in textures
-}
-
-__forceinline void calculateBarycentricCoordinates3D(const Vec4_f32x16& P, const Vec4_f32x16& A, const Vec4_f32x16& B, const Vec4_f32x16& C, float32x16& alpha, float32x16& beta, float32x16& gamma)
-{
-	/* //this version is less precise, causes texture issues in some places
-	Vec4_f32x16 v0 = B - A;
-	Vec4_f32x16 v1 = C - A;
-	Vec4_f32x16 v2 = P - A;
-
-	float32x16 d00 = v0.dot3d(v0);
-	float32x16 d01 = v0.dot3d(v1);
-	float32x16 d11 = v1.dot3d(v1);
-	float32x16 d20 = v2.dot3d(v0);
-	float32x16 d21 = v2.dot3d(v1);
-	float32x16 den = d00 * d11 - (d01 * d01);
-	beta = (d11 * d20 - d01 * d21) / den;
-	gamma = (d00 * d21 - d01 * d20) / den;
-	alpha = float32x16(1) - beta - gamma; //doesn't seem to hurt calculating it like this*/
-
-	Vec4_f32x16 n = (B - A).cross3d(C - A);
-	alpha = ((B - P).cross3d(C - P)).dot3d(n) / n.dot3d(n);
-	beta  = ((C - P).cross3d(A - P)).dot3d(n) / n.dot3d(n);
-	gamma = ((A - P).cross3d(B - P)).dot3d(n) / n.dot3d(n);
-}
-
-Vec4_f32x16 mask_load_vec4_f32x16_from_framebuffer(const void* frameBuffer, int x, int y, int w, Mask16 mask)
-{
-	int loadInd = y * w + x;
-	const int64_t* p = (const int64_t*)frameBuffer;
-	__m512i rgba0_7 = _mm512_maskz_loadu_epi64(mask, p + loadInd);
-	__m512i rgba8_15 = _mm512_maskz_loadu_epi64(mask >> 8, p + loadInd + 8);
-
-	__m512i r0_15_g0_15_ph = _mm512_permutex2var_epi16(rgba0_7, _mm512_setr_epi16(0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 1, 5, 9, 13, 17, 21, 25, 29, 33, 37, 41, 45, 49, 53, 57, 61), rgba8_15);
-	__m512i b0_15_a0_15_ph = _mm512_permutex2var_epi16(rgba0_7, _mm512_setr_epi16(2, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46, 50, 54, 58, 62, 3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 43, 47, 51, 55, 59, 63), rgba8_15);
-
-	Vec4_f32x16 ret;
-	ret.r = _mm512_cvtph_ps(_mm512_extracti32x8_epi32(r0_15_g0_15_ph, 0));
-	ret.g = _mm512_cvtph_ps(_mm512_extracti32x8_epi32(r0_15_g0_15_ph, 1));
-	ret.b = _mm512_cvtph_ps(_mm512_extracti32x8_epi32(b0_15_a0_15_ph, 0));
-	ret.a = _mm512_cvtph_ps(_mm512_extracti32x8_epi32(b0_15_a0_15_ph, 1));
-	return ret;
-}
-
 void RasterizingRenderer::drawTriangleBatch(const PixelStageInput& inp, const int threadIndex)
 {
 	const auto& drawCmd = *inp.cmd;
--- src/Renderers/RendererBase.h
+++ src/Renderers/RendererBase.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include "../C_Input.h"
 #include "../Vec.h"
+#include "../helpers.h"
 #undef min
 #undef max
 struct GameSettings;
@@ -19,6 +20,10 @@ public:
 	//virtual void handleInputEvent(const SDL_Event& ev, C_Input& input) = 0;
 	virtual void loadScene(RendererLoadSceneData scd) = 0;
 	virtual void renderFrame(const GameSettings& settings) = 0;
+
+	static void calculateBarycentricCoordinates2D(const Vec4_f32x16& r, const Vec4_f32x16& r1, const Vec4_f32x16& r2, const Vec4_f32x16& r3, const float32x16& rcpSignedArea, float32x16& alpha, float32x16& beta, float32x16& gamma);
+	static void calculateBarycentricCoordinates3D(const Vec4_f32x16& P, const Vec4_f32x16& A, const Vec4_f32x16& B, const Vec4_f32x16& C, float32x16& alpha, float32x16& beta, float32x16& gamma);
 	static void mask_store_vec4_f32x16_to_framebuffer(const Vec4_f32x16& pack, void* frameBuffer, int x, int y, int w, Mask16 mask);
+	static Vec4_f32x16 mask_load_vec4_f32x16_from_framebuffer(const void* frameBuffer, int x, int y, int w, Mask16 mask);
 private:
 };
