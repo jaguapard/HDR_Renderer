8b8ec16ae7e2850312cc92509028fb068182eb24
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-05-10 10:52:05 +0500

    mask_store_rows_512_to_4x128_ps, mask_load_rows_4x128_to_512_ps in RendererBase

--- src/Renderers/RendererBase.h
+++ src/Renderers/RendererBase.h
@@ -30,6 +30,32 @@ public:
 	static void calculateBarycentricCoordinates3D(const Vec4_f32x8& P, const Vec4_f32x8& A, const Vec4_f32x8& B, const Vec4_f32x8& C, std::array<float32x8, 3>& outBarycentrics);
 	static void mask_store_vec4_f32x16_to_framebuffer(const Vec4_f32x16& pack, void* frameBuffer, int x, int y, int w, Mask16 mask);
 	static Vec4_f32x16 mask_load_vec4_f32x16_from_framebuffer(const void* frameBuffer, int x, int y, int w, Mask16 mask);
+	
+	static __forceinline void mask_store_rows_512_to_4x128_ps(__m512 value, __mmask16 mask, void* dst, uint32_t xStart, uint32_t yStart, uint32_t w)
+	{
+		__m128 v0 = _mm512_extractf32x4_ps(value, 0);
+		__m128 v1 = _mm512_extractf32x4_ps(value, 1);
+		__m128 v2 = _mm512_extractf32x4_ps(value, 2);
+		__m128 v3 = _mm512_extractf32x4_ps(value, 3);
+		float* p = (float*)dst;
+		_mm_mask_storeu_ps(p + yStart * w + xStart, mask, v0);
+		_mm_mask_storeu_ps(p + (1+yStart) * w + xStart, mask >> 4, v1);
+		_mm_mask_storeu_ps(p + (2+yStart) * w + xStart, mask >> 8, v2);
+		_mm_mask_storeu_ps(p + (3+yStart) * w + xStart, mask >> 12, v3);
+	}
+	static __forceinline __m512 mask_load_rows_4x128_to_512_ps(__mmask16 mask, const void* src, uint32_t xStart, uint32_t yStart, uint32_t w)
+	{
+		const float* p = (const float*)src;
+		__m128 v0 = _mm_maskz_loadu_ps(mask, p + yStart * w + xStart);
+		__m128 v1 = _mm_maskz_loadu_ps(mask >> 4, p + (yStart + 1) * w + xStart);
+		__m128 v2 = _mm_maskz_loadu_ps(mask >> 8, p + (yStart + 2) * w + xStart);
+		__m128 v3 = _mm_maskz_loadu_ps(mask >> 12, p + (yStart + 3) * w + xStart);
+		__m512 ret = _mm512_castps128_ps512(v0);
+		ret = _mm512_insertf32x4(ret, v1, 1);
+		ret = _mm512_insertf32x4(ret, v2, 2);
+		ret = _mm512_insertf32x4(ret, v3, 3);
+		return ret;
+	}
 
 	//Writes out colors to pixels (x,y) of the framebuffer using mask
 	static void scatterToFrameBuffer(const Vec4_f32x16& colors, int32x16 x, int32x16 y, Mask16 mask, void* frameBuf, int framebufW);
