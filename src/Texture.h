cad5678d8a6bd7223fa8d48a1d427d21032f7341
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-05-17 00:26:58 +0500

    trying to consexpr LUT

--- src/Texture.h
+++ src/Texture.h
@@ -1,7 +1,17 @@
 #pragma once
 #include <vector>
 #include <SDL3/SDL.h>
+#include <array>
 
+constexpr static std::array<int16_t, 256> toLinear_FP16 = []() {
+	std::array<int16_t, 256> result;
+	for (int i = 0; i < 256; ++i)
+	{
+		float f32 = pow(i / 255.0, 2.2);
+		result[i] = _mm_extract_epi16(_mm_cvtps_ph(_mm_set1_ps(f32), _MM_FROUND_TO_NEAREST_INT), 0);
+	}
+	return result;
+	}();
 struct MipLevel
 {
 	std::vector<uint32_t> colors;
