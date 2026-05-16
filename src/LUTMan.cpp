d52ee2cbf6aa2d4e3f1d000504f0801895dc9e73
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-05-17 00:36:05 +0500

    rgbToLinear_fp16 const LUT in LUTMan

--- src/LUTMan.cpp
+++ src/LUTMan.cpp
@@ -1,6 +1,16 @@
 #define _USE_MATH_DEFINES
 #include "LUTMan.h"
 #include <cmath>
+
+alignas(64) const std::array<int16_t, 256> LUTMan::rgbToLinear_fp16 = []() {
+	std::array<int16_t, 256> result;
+	for (int i = 0; i < 256; ++i)
+	{
+		float f32 = pow(i / 255.0, 2.2);
+		result[i] = _mm_extract_epi16(_mm_cvtps_ph(_mm_set1_ps(f32), _MM_FROUND_TO_NEAREST_INT), 0);
+	}
+	return result;
+	}();
 void LUTMan::init()
 {
 	//for (int i = 0; i < sineLUT_fp32.size(); ++i) sineLUT_fp32[i] = std::sin(M_PI)
--- src/LUTMan.h
+++ src/LUTMan.h
@@ -9,6 +9,8 @@ public:
 	static float32x16 sin(float32x16 x);
 	static float32x16 cos(float32x16 x);
 	static float32x16 log2(float32x16 x);
+
+	static const std::array<int16_t, 256> rgbToLinear_fp16; //unlike other LUTs here, this is exact aside rounding differences
 private:
 	static inline std::array<float, 32> sineLUT_fp32, cosLUT_fp32;
 };
