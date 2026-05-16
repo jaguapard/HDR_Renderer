4dd2da4eafb4c9658399714371e78512f87357ea
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-05-17 00:23:18 +0500

    wip new texture

---
+++ src/Texture.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <vector>
+#include <SDL3/SDL.h>
+
+struct MipLevel
+{
+	std::vector<uint32_t> colors;
+	uint32_t w, h;
+};
+
+class Texture
+{
+public:
+	Texture(const SDL_Surface* s);
+
+private:
+	std::vector<MipLevel> mipLevels;
+};
