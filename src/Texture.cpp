2130dc10947e968e88b72ce62137607eab2df227
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-05-17 22:50:20 +0500

    MipLevel::gatherA

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -14,6 +14,50 @@ MipLevel::MipLevel(uint32_t w, uint32_t h)
     this->opacityMap.shrink_to_fit();
 }
 
+[[gnu::target("avx512vbmi")]]
+Vec4_f32x16 MipLevel::gatherLinearIntensities(float32x16 u, float32x16 v, Mask16 mask) const
+{
+    auto [pixelsX, pixelsY] = this->mapper.UV_to_XY(u, v);
+    float32x16 lerpT_x = pixelsX - float32x16(_mm512_floor_ps(pixelsX));
+    float32x16 lerpT_y = pixelsY - float32x16(_mm512_floor_ps(pixelsY));
+    int32x16 startX = pixelsX.trunc();
+    int32x16 startY = pixelsY.trunc();
+    const auto& p = this->mapper.getParams();
+
+    std::array<Vec4_f32x16, 4> linear;
+    for (int i = 0; i < 4; ++i)
+    {
+        int sx = i % 2;
+        int sy = i / 2;
+        int32x16 sampleX = startX + sx, sampleY = startY + sy;
+        this->mapper.wrapInts(sampleX, sampleY);
+
+        int32x16 samples = int32x16::gather(this->colors.data(), sampleY * p.w + sampleX, mask);
+        linear[i] = Decoder::RGBA8888_to_linear_using_FP16_LUT(samples);
+    }
+
+    Vec4_f32x16 lerp1 = lerp(linear[0], linear[1], lerpT_x);
+    Vec4_f32x16 lerp2 = lerp(linear[2], linear[3], lerpT_x);
+    return lerp(lerp1, lerp2, lerpT_y);
+}
+
+float32x16 MipLevel::gatherA(float32x16 u, float32x16 v, Mask16 mask) const
+{
+    auto [pixelsX, pixelsY] = this->mapper.UV_to_XY(u, v);
+    //TODO: same filtering for opacity maps as textures, else it creates disagreement between stages
+    int32x16 sx = pixelsX.trunc();
+    int32x16 sy = pixelsY.trunc();
+    this->mapper.wrapInts(sx, sy);
+
+    int32x16 ind = sy * this->mapper.getParams().w + sx;
+    int32x16 gatherInd = ind >> 5;
+    int32x16 shifts = ind & 31;
+    int32x16 gathered = int32x16::gather(this->opacityMap.data(), gatherInd, mask);
+    gathered &= int32x16(1) << shifts;
+    return _mm512_mask_mov_ps(float32x16(0.f), gathered != 0, float32x16(1.f));
+}
+
+
 Texture::Texture(const SDL_Surface* s)
 {
     int w = s->w;
@@ -76,29 +120,7 @@ Texture::Texture(const SDL_Surface* s)
     }
 }
 
-[[gnu::target("avx512vbmi")]]
-Vec4_f32x16 MipLevel::gatherLinearIntensities(float32x16 u, float32x16 v, Mask16 mask) const
+Vec4_f32x16 Texture::gatherLinearIntensities(float32x16 u, float32x16 v, Mask16 mask) const
 {
-    auto [pixelsX, pixelsY] = this->mapper.UV_to_XY(u, v);
-    float32x16 lerpT_x = pixelsX - float32x16(_mm512_floor_ps(pixelsX));
-    float32x16 lerpT_y = pixelsY - float32x16(_mm512_floor_ps(pixelsY));
-    int32x16 startX = pixelsX.trunc();
-    int32x16 startY = pixelsY.trunc();
-    const auto& p = this->mapper.getParams();
-
-    std::array<Vec4_f32x16, 4> linear;
-    for (int i = 0; i < 4; ++i)
-    {
-        int sx = i % 2;
-        int sy = i / 2;
-        int32x16 sampleX = startX + sx, sampleY = startY + sy;
-        this->mapper.wrapInts(sampleX, sampleY);
-
-        int32x16 samples = int32x16::gather(this->colors.data(), sampleY * p.w + sampleX, mask);
-        linear[i] = Decoder::RGBA8888_to_linear_using_FP16_LUT(samples);
-    }
-
-    Vec4_f32x16 lerp1 = lerp(linear[0], linear[1], lerpT_x);
-    Vec4_f32x16 lerp2 = lerp(linear[2], linear[3], lerpT_x);
-    return lerp(lerp1, lerp2, lerpT_y);
+    return this->mipLevels[0].gatherLinearIntensities(u, v, mask);
 }
