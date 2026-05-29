0fb0bafc6fa9172f1fd3e62ed9220ca0ab795e4b
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-05-29 17:03:07 +0500

    Creating hardware rasterizer in main as default

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "C_Input.h"
 #include "Renderers\RayCasting\RayCastingRenderer.h"
 #include "Renderers\Rasterizing\RasterizingRenderer.h"
+#include "Renderers\HardwareRasterizingRenderer.h"
 #include "GameSettings.h"
 #include <bob/Matrix4.h>
 #include "Threadpool.h"
@@ -142,7 +143,7 @@ int main(int argc, char* argv[])
             for (auto& it : Statsman::statsmenForThreads) it.reset();
             if (!currentRenderer && !scheduledRendererChange)
             {
-                scheduledRendererChange = std::make_shared<RasterizingRenderer>();
+                scheduledRendererChange = std::make_shared<HardwareRasterizingRenderer>();
                 sceneReloadNeeded = true;
                 skipThisFrame = true;
                 //else if (dynamic_cast<RasterizingRenderer*>(currentRenderer.get())) scheduledRendererChange = std::make_shared<RayCastingRenderer>();
@@ -207,8 +208,6 @@ int main(int argc, char* argv[])
                 continue;
             }
 
-            D3D11_MAPPED_SUBRESOURCE mapped = graphics.CPURendering_OnFrameStart(cpuRenderingCtx);
-
             //TODO: change coordinate system, so Y = up/down, Z = into the screen, X = left/right
             Matrix4 rotation = Matrix4::rotationXYZ(gs.camAng);
 
@@ -244,7 +243,12 @@ int main(int argc, char* argv[])
             if (inp.wasButtonPressedOnThisFrame(SDL_SCANCODE_T)) gs.texturingEnabled ^= 1;
 
             //std::cout << "cam pos" << vec2str(gs.camPos) << ", camAng: " << vec2str(gs.camAng) << "\n";
-            gs.graphicsOutputBuffer = mapped.pData;
+            bool isHardwareRenderer = dynamic_pointer_cast<HardwareRasterizingRenderer>(currentRenderer) != nullptr;
+            if (!isHardwareRenderer)
+            {
+                D3D11_MAPPED_SUBRESOURCE mapped = graphics.CPURendering_OnFrameStart(cpuRenderingCtx);
+                gs.graphicsOutputBuffer = mapped.pData;
+            }
             currentRenderer->renderFrame(gs);
             osd.registerFrameDone(currentRenderer.get());
 
@@ -256,7 +260,7 @@ int main(int argc, char* argv[])
                 {"Output resolution", std::to_string(graphics.w) + "x" + std::to_string(graphics.h) },
                 {"OSD draw time: ", std::to_string(lastOsdDrawMs) + " ms"},
             };
-            if (gs.osdEnabled) //VERY slow, impacts FPS a lot, disabled by default
+            if (gs.osdEnabled && !isHardwareRenderer) //VERY slow, impacts FPS a lot, disabled by default
             {
                 float scalingFactor = 1;// std::max(0.5f, float(texDesc.Height) / h); //very small OSD becomes unreadable
                 auto osdSurface = osd.draw(scalingFactor, additionalInfo);
@@ -293,9 +297,9 @@ int main(int argc, char* argv[])
                     std::cout << std::string(nls+1, '\r');*/
                 }
             }
+            if (!isHardwareRenderer) graphics.CPURendering_Present(cpuRenderingCtx);
             uint64_t ticksAfterOSD = SDL_GetTicksNS();
             lastOsdDrawMs = (ticksAfterOSD - ticksBeforeOSD) / 1e6;
-            graphics.CPURendering_Present(cpuRenderingCtx);
         }
     }
     catch (const std::exception& e)
