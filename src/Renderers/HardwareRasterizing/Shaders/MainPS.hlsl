f865a487d33cd00ad9e6209c131775e032c4353d
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-05-29 20:07:49 +0500

    Clip pixels with alpha below 0.1 in pixel shader

--- src/Renderers/HardwareRasterizing/Shaders/MainPS.hlsl
+++ src/Renderers/HardwareRasterizing/Shaders/MainPS.hlsl
@@ -4,6 +4,8 @@ Texture2D tex;
 SamplerState sam;
 float4 main(float3 worldPos : WorldPos, float2 uv : TEXCOORD0, float3 normals : TEXCOORD1) : SV_Target
 {
-    return tex.Sample(sam, uv);
+    float4 s = tex.Sample(sam, uv);
+    clip(s.a - 0.1);
+    return s;
     //return saturate(length(worldPos - camPos) / 1500);
 }
