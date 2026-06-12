50b163f04c98e592cdea2a1b5d56d99fe4073b56
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-12 07:06:12 +0500

    vec_cvt -> vcvt

--- src/Mappers.cpp
+++ src/Mappers.cpp
@@ -34,7 +34,7 @@ int32x16 WrappingMapper::wrapIntWithRcp(int32x16 x, uint64_t rcp, uint32_t v)
 int32x16 WrappingMapper::wrapPositiveIntWithRcp(u32x16 x, uint64_t rcp, uint32_t v)
 {
 	for (int i = 0; i < 16; ++i) assert(x[i] >= 0);
-	auto x64 = vec_cvt<uint64_t>(x);
+	auto x64 = vcvt<uint64_t>(x);
 	auto div = (x64 * rcp) >> 32;
 	auto rem = x64 - (div * v);
 	return permx2(reinterpret<u32x16>(rem.lo), u32x16(0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30), reinterpret<u32x16>(rem.hi));
